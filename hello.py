7767432b0452bcae432e805ceace328f5b0b917d
Nick Friesen <freeze@Fr33ze.localdomain>
2020-06-05 18:03:25 -0600

    Added a comment line in vim

--- hello.py
+++ hello.py
@@ -5,3 +5,4 @@ def hello():
 
 hello()
 print("This is a hello from our friend 'Python' who is going to be joining us on this journey of Github :)")
+#This is a comment that I am making, from vim, in a bash terminal on my local machine
